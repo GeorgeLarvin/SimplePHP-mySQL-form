1bce23fa899020bf3ef3f819150b796e769ac1cf
George Larvin <georgelarvin@hotmail.co.uk>
2016-07-27 15:40:43 +0100

    Add files via upload
    updated all working fine so far still functionality and css work to do

--- connection.php
+++ connection.php
@@ -2,20 +2,20 @@
 	
 $hostname = 'localhost';
 $username = 'root';
-$password = '';
+$password1 = '';
 $dbname = 'myfirstdatabase';	
 	
 	
 	
 	//making a connection to mySQl database
 
-$dbc = mysqli_connect($hostname, $username, $password, $dbname) OR die('could not connect ERROR: '.mysqli_connection_error());
+$dbc = mysqli_connect($hostname, $username, $password1, $dbname) OR die('could not connect ERROR: '.mysqli_connection_error());
 
 	//set encoding/
 mysqli_set_charset($dbc,"UTF8");	
 	
 	if($dbc) {
-		echo "your connected too ".$dbname;
+		
 	}
 	
 	
--- lesson18.creatingaform.php
+++ lesson18.creatingaform.php
@@ -1,3 +1,46 @@
+<?php
+
+
+	
+if ($_SERVER['REQUEST_METHOD']== 'POST'){
+	
+$fname = $_POST["fname"];	
+$lname = $_POST["lname"];	
+$email = $_POST["email"];	
+$gender = $_POST["gender"];	
+$age = $_POST["age"];	
+$comments = $_POST["comments"];	
+$password = $_POST=['password'];		
+	
+	if (!empty($fname) && !empty($lname) && !empty($email) && !empty($gender) && !empty($age) && !empty($comments) && !empty($password)) {
+		
+		include("connection.php");
+		
+		mysqli_query($dbc, "INSERT INTO users(first_name, last_name, email, gender, age, comments, password) VALUES('$fname','$lname','$email','$gender','$age','$comments','$password')");
+		
+		$registered = mysqli_affected_rows($dbc);
+		
+		echo $registered. " row is affected"; 
+		
+	} else {
+		
+		echo "<h3><em><u><p style='color:red'>please fill all values of the form</p></u></em></h3>";
+		
+	}
+		
+	
+}else {
+	
+	echo "<h3><em><u>Please enter your details below</u></em></h3>";
+	
+}	
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +49,7 @@
 <body>
 
 <!-- start of form -->
-<form action="processing.php" method="post">
+<form action="lesson18.creatingaform.php" method="post">
 	
 	<p>First name : <input type="text" name="fname" size="20" maxlength="40" /></p>
 	<p>Last name : <input type="text" name="lname" size="20" maxlength="40" /></p>
@@ -16,7 +59,7 @@
 	<br />
 	<p>Age : <select name="age">
 			<option value="0-20">Under  20</option>
-			<option value="20-40">20 or over</option>
+			<option value="20-40">20 to 39</option>
 			<option value="40-100">40 or over</option>
 	</select></p>	
 	<p>Comments : <br /><textarea name="comments" rows="3" cols="40" maxlength="150" ></textarea>
